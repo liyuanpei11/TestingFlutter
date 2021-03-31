716f01798b0bc2ae18787787bb6e91071f7ed15f
Roy Adler <royadler@outlook.com>
2021-03-31 11:59:07 +0200

    LoginScreen wird vernünftig angezeigt

--- lib/api/request_response_handler.dart
+++ lib/api/request_response_handler.dart
@@ -4,7 +4,7 @@ import 'debug.dart';
 import 'preferences.dart';
 
 class RequestResponseHandler {
-  static String pr0Api = 'https://pr0gramm.com/api';
+  static const String pr0Api = 'pr0gramm.com';
   Map<String, String> headers = {};
 
   String paramsMaker(List<String> stringList) {
@@ -16,11 +16,6 @@ class RequestResponseHandler {
   }
 
   Future<http.Response> itemsGetWithoutPermission() async {
-    print("asd");
-    // final a = await get(url: "/items/get");
-
-    // print(a);
-
     return get(url: "/items/get");
   }
 
@@ -94,42 +89,18 @@ class RequestResponseHandler {
   }
 
   Future<http.Response> get({String url}) async {
-    var url = Uri.https('www.googleapis.com', '/books/v1/volumes');
+    Uri uri = Uri.https(pr0Api, 'api' + url);
+
     // var url = Uri.https('www.pr0gramm.com', '/api/items/get');
 
     // Await the http get response, then decode the json-formatted response.
-    var response = await http.get(url);
+    var response = await http.get(uri, headers: headers);
     if (response.statusCode == 200) {
-      print('Response body: ${response.body}');
       return response;
     } else {
-      print('Response body: ${response.body}');
       print('Request failed with status: ${response.statusCode}.');
       return null;
     }
-    // String requestPath = pr0Api + url;
-    // Uri request;
-    // try {
-    //   String proApi = 'www.google.de';
-    //   request = new Uri.https(proApi, "/helloworld"); // TODO: Test ob das geht
-    //   print(request);
-    //   if (internetDEBUG) {
-    //     print("Request: " + request.path.toString());
-    //     if (headers != null) {
-    //       print("Header: " + headers.toString());
-    //     }
-    //   }
-    //   print("DEBUG LI");
-    //   return await http.get(Uri.https(proApi, "/helloworld"));
-    // http.Response response = await http.get(request, headers: headers);
-    // print("DEBUG LI");
-    // print(response.body);
-    // print("DEBUG LI");
-    // return response;
-    // } catch (e) {
-    //   print(e);
-    //   return null;
-    // }
   }
 
   setCookie(String cookies) async {
